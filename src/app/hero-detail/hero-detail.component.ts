952271136f9a5b7bd7a00e61c01f3ca344164fce
rakeen01 <rakeen.murtaza.12@gmail.com>
2021-02-08 20:07:31 +0600

    Finished the app!

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,18 +1,46 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { switchMap } from "rxjs/operators" // RxJS v6
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css']
+  styleUrls: [ './hero-detail.component.css' ]
 })
 export class HeroDetailComponent implements OnInit {
+  hero: Hero;
 
-  @Input() hero: Hero;
-
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
 
   ngOnInit(): void {
+    this.getHero();
   }
 
+  getHero(): void {
+
+    const id = +this.route.snapshot.params.id;
+
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+
+  }
+
+  // Update/Save hero
+
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
